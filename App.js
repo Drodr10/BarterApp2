41f48d3eba527d96d6731758dca3cb0c7dd8889f
Drodr10 <69482445+Drodr10@users.noreply.github.com>
2021-01-13 16:42:27 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import WelcomeScreen from './screens/WelcomeScreen';
+import WelcomeScreen from './screens/SignupLoginScreen';
 
 export default class App extends React.Component{
   render(){
     return(
-      <WelcomeScreen/>
+      <SignupLoginScreen/>
     );
   }
 }
